099d75e0c21a2464c14def4d5417334ecbeca039
MahnuddinMursalim <mahmuddinmursalim5@gmail.com>
2025-11-10 15:34:28 +0800

    Add files via upload

--- ARRAY.py
+++ ARRAY.py
@@ -1,28 +1,29 @@
-# MAHMUDDIN MURSALIM
-# D0425529
-# ARRAY
+# NAMA		: MAHMUDDIN MURSALIM
+# NIM			: D0425529
+# KELAS		: B SISTEM INFORMASI
+# PROGRAM ARRAY
 
 mahasiswa = ["ibrahim", "kurniawan", "jona irwansyah", "nopal"]
 
 # Menampilkan semua nama
-print("Daftar Mahasiswa:", mahasiswa)
+print("Daftar Mahasiswa : ", mahasiswa)
 
 # Mengakses 
-print("Mahasiswa pertama:", mahasiswa[0])
+print("Mahasiswa pertama : ", mahasiswa[0])
 
 #Mengakses
-print("Mahasiswa kedua:", mahasiswa[1])
+print("Mahasiswa kedua : ", mahasiswa[1])
 
 #Mengakses
-print("Mahasiswa ketiga:", mahasiswa[2])
+print("Mahasiswa ketiga : ", mahasiswa[2])
 
 #Mengakses
-print("Mahasiswa keempat:", mahasiswa[3])
+print("Mahasiswa keempat : ", mahasiswa[3])
 
 # Menambah data 
 mahasiswa.append("muh alim wisjaya")
-print("Setelah ditambah:", mahasiswa)
+print("Setelah ditambah : ", mahasiswa)
 
 # Menghapus 
 mahasiswa.remove("jona irwansyah")
-print("Setelah jona irwansyah dihapus :", mahasiswa)
+print("Setelah jona irwansyah dihapus : ", mahasiswa)
